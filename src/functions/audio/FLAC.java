4df1d0f334e7b57cc7be5f307df26eec8eb5a3c6
Pacifico Paul <46673657+paulpacifico@users.noreply.github.com>
2020-09-10 10:31:46 +0200

    Add files via upload

--- src/functions/audio/FLAC.java
+++ src/functions/audio/FLAC.java
@@ -24,7 +24,6 @@ import java.awt.Desktop;
 import java.io.File;
 import java.io.IOException;
 import javax.swing.JOptionPane;
-
 import application.Ftp;
 import application.Settings;
 import application.Shutter;
@@ -70,7 +69,7 @@ public class FLAC extends Shutter {
 						lblEncodageEnCours.setText(file.getName());
 						tempsRestant.setVisible(false);
 						btnStart.setEnabled(false);
-						btnAnnuler.setEnabled(true);
+						btnCancel.setEnabled(true);
 						comboFonctions.setEnabled(false);
 						
 						long fileSize = 0;
@@ -96,13 +95,13 @@ public class FLAC extends Shutter {
 							progressBar1.setIndeterminate(false);
 							lblEncodageEnCours.setText(language.getProperty("lblEncodageEnCours"));
 							btnStart.setEnabled(true);
-							btnAnnuler.setEnabled(false);
+							btnCancel.setEnabled(false);
 							comboFonctions.setEnabled(true);
 							break;
 						}
 						
 						progressBar1.setIndeterminate(false);
-						btnAnnuler.setEnabled(false);
+						btnCancel.setEnabled(false);
 		            }
 		           //SCANNING
 		            
@@ -204,13 +203,58 @@ public class FLAC extends Shutter {
 
 	private static String setAudio(String audio) {
 		
-		String audioSpeed = "";
+        //Fade-in
+		String audioFilter = ""; 		
+		if (caseAudioFadeIn.isSelected())
+		{ 
+			long audioInValue = (long) (Integer.parseInt(spinnerAudioFadeIn.getText()) * ((float) 1000 / FFPROBE.currentFPS));
+			long audioStart = 0;
+			
+			if (caseInAndOut.isSelected() && VideoPlayer.comboMode.getSelectedItem().toString().contentEquals(Shutter.language.getProperty("cutUpper")))
+			{
+				long totalIn = (long) (Integer.parseInt(VideoPlayer.caseInH.getText()) * 3600000 + Integer.parseInt(VideoPlayer.caseInM.getText()) * 60000 + Integer.parseInt(VideoPlayer.caseInS.getText()) * 1000 + Integer.parseInt(VideoPlayer.caseInF.getText()) * (1000 / FFPROBE.currentFPS));
+				
+				if (totalIn >= 10000)
+					audioStart = 10000;
+				else
+					audioStart = (long) (Integer.parseInt(VideoPlayer.caseInH.getText()) * 3600000 + Integer.parseInt(VideoPlayer.caseInM.getText()) * 60000 + Integer.parseInt(VideoPlayer.caseInS.getText()) * 1000 + Integer.parseInt(VideoPlayer.caseInF.getText()) * (1000 / FFPROBE.currentFPS));
+			}
+
+			audioFilter += ",afade=in:st=" + audioStart + "ms:d=" + audioInValue + "ms";
+		}
+		
+		//Fade-out
+		if (caseAudioFadeOut.isSelected())
+		{
+			long audioOutValue = (long) (Integer.parseInt(spinnerAudioFadeOut.getText()) * ((float) 1000 / FFPROBE.currentFPS));
+			long audioStart = (long) FFPROBE.dureeTotale - audioOutValue;
+			
+			if (caseInAndOut.isSelected())
+			{
+				long totalIn = (long) (Integer.parseInt(VideoPlayer.caseInH.getText()) * 3600000 + Integer.parseInt(VideoPlayer.caseInM.getText()) * 60000 + Integer.parseInt(VideoPlayer.caseInS.getText()) * 1000 + Integer.parseInt(VideoPlayer.caseInF.getText()) * (1000 / FFPROBE.currentFPS));
+				long totalOut = (long) (Integer.parseInt(VideoPlayer.caseOutH.getText()) * 3600000 + Integer.parseInt(VideoPlayer.caseOutM.getText()) * 60000 + Integer.parseInt(VideoPlayer.caseOutS.getText()) * 1000 + Integer.parseInt(VideoPlayer.caseOutF.getText()) * (1000 / FFPROBE.currentFPS));
+				 
+				if (VideoPlayer.comboMode.getSelectedItem().toString().contentEquals(Shutter.language.getProperty("cutUpper")))
+				{
+					if (totalIn >= 10000)
+						audioStart = 10000 + (totalOut - totalIn) - audioOutValue;
+					else
+						audioStart = (long) (Integer.parseInt(VideoPlayer.caseOutH.getText()) * 3600000 + Integer.parseInt(VideoPlayer.caseOutM.getText()) * 60000 + Integer.parseInt(VideoPlayer.caseOutS.getText()) * 1000 + Integer.parseInt(VideoPlayer.caseOutF.getText()) * (1000 / FFPROBE.currentFPS)) - audioOutValue;
+				}
+				else //Remove mode
+					audioStart = FFPROBE.dureeTotale - (totalOut - totalIn) - audioOutValue;
+			}
+			
+			audioFilter += ",afade=out:st=" + audioStart + "ms:d=" + audioOutValue + "ms";
+		}
+		
+    	//Audio Speed
 		if (caseConvertAudioFramerate.isSelected())     
         {
         	float AudioFPSIn = Float.parseFloat((comboAudioIn.getSelectedItem().toString()).replace(",", "."));
         	float AudioFPSOut = Float.parseFloat((comboAudioOut.getSelectedItem().toString()).replace(",", "."));
         	float value = (float) (AudioFPSOut / AudioFPSIn);
-        	audioSpeed = ",atempo=" + value;	
+        	audioFilter += ",atempo=" + value;	
         }
 		
 		if (caseMixAudio.isSelected() && lblMix.getText().equals(language.getProperty("stereo")))						
@@ -221,7 +265,7 @@ public class FLAC extends Shutter {
 			}
 			
 			if (FFPROBE.stereo)
-				audio += "-filter_complex amerge=inputs=" + liste.size() + audioSpeed + " -ac 2 ";
+				audio += "-filter_complex amerge=inputs=" + liste.size() + audioFilter + " -ac 2 ";
 			else
 			{
 				audio += "-filter_complex " + '"';
@@ -244,25 +288,35 @@ public class FLAC extends Shutter {
 				}
 				audio += left + "amerge=inputs=" + cl + ",channelmap=map=FL[left];" + right + "amerge=inputs=" + cr + ",channelmap=map=FR[right];";
 						
-				audio += "[left][right]amerge=inputs=2" + audioSpeed + "[out]" + '"' + " -map " + '"' + "[out]" + '"' + " -ac 2 ";
+				audio += "[left][right]amerge=inputs=2" + audioFilter + "[out]" + '"' + " -map " + '"' + "[out]" + '"' + " -ac 2 ";
 			}							
 		}
 		else if (caseMixAudio.isSelected() && lblMix.getText().equals("5.1"))
 		{
-			audio = "-filter_complex " + '"' + "[0:a][1:a][2:a][3:a][4:a][5:a]join=inputs=6:channel_layout=5.1" + audioSpeed + "[a]" + '"' + " -map " + '"' + "[a]" + '"' + " ";
+			audio = "-filter_complex " + '"' + "[0:a][1:a][2:a][3:a][4:a][5:a]join=inputs=6:channel_layout=5.1" + audioFilter + "[a]" + '"' + " -map " + '"' + "[a]" + '"' + " ";
+		}
+		else if (caseMixAudio.isSelected() && lblMix.getText().equals(language.getProperty("mono")))						
+		{
+			for (int n = 1 ; n < liste.size() ; n++)
+			{
+				audio += "-i " + '"' + liste.elementAt(n) + '"' + " ";
+			}
+			
+			audio += "-filter_complex amerge=inputs=" + liste.size() + audioFilter + " -ac 1 ";
+			
 		}
 		else if (FFPROBE.stereo)
 		{
 			audio = "-map a:0 ";
-			if (caseConvertAudioFramerate.isSelected())     
-				audio += audioSpeed.replace(",", " -filter_complex ") + " ";
+			if (audioFilter != "")     
+				audio += audioFilter.replaceFirst(",", " -filter_complex ") + " ";
 		}
 		else if (FFPROBE.channels > 1)
-	    	audio = "-filter_complex " + '"' + "[0:a:0][0:a:1]amerge=inputs=2" + audioSpeed + "[a]" + '"' + " -map " + '"' + "[a]" + '"' + " ";	
+	    	audio = "-filter_complex " + '"' + "[0:a:0][0:a:1]amerge=inputs=2" + audioFilter + "[a]" + '"' + " -map " + '"' + "[a]" + '"' + " ";	
 		else //Fichier Mono
 		{
-			if (caseConvertAudioFramerate.isSelected())     
-				audio += audioSpeed.replace(",", " -filter_complex ") + " ";
+			if (audioFilter != "")     
+				audio += audioFilter.replaceFirst(",", " -filter_complex ") + " ";
 		}
 			
 		//Quantization
@@ -277,16 +331,61 @@ public class FLAC extends Shutter {
 	
 	private static void splitAudio(String fichier, String extension, File file, String sortie) throws InterruptedException {
 		
-		String audioSpeed = "";
+        //Fade-in
+		String audioFilter = ""; 		
+		if (caseAudioFadeIn.isSelected())
+		{ 
+			long audioInValue = (long) (Integer.parseInt(spinnerAudioFadeIn.getText()) * ((float) 1000 / FFPROBE.currentFPS));
+			long audioStart = 0;
+			
+			if (caseInAndOut.isSelected() && VideoPlayer.comboMode.getSelectedItem().toString().contentEquals(Shutter.language.getProperty("cutUpper")))
+			{
+				long totalIn = (long) (Integer.parseInt(VideoPlayer.caseInH.getText()) * 3600000 + Integer.parseInt(VideoPlayer.caseInM.getText()) * 60000 + Integer.parseInt(VideoPlayer.caseInS.getText()) * 1000 + Integer.parseInt(VideoPlayer.caseInF.getText()) * (1000 / FFPROBE.currentFPS));
+				
+				if (totalIn >= 10000)
+					audioStart = 10000;
+				else
+					audioStart = (long) (Integer.parseInt(VideoPlayer.caseInH.getText()) * 3600000 + Integer.parseInt(VideoPlayer.caseInM.getText()) * 60000 + Integer.parseInt(VideoPlayer.caseInS.getText()) * 1000 + Integer.parseInt(VideoPlayer.caseInF.getText()) * (1000 / FFPROBE.currentFPS));
+			}
+
+			audioFilter += ",afade=in:st=" + audioStart + "ms:d=" + audioInValue + "ms";
+		}
+		
+		//Fade-out
+		if (caseAudioFadeOut.isSelected())
+		{
+			long audioOutValue = (long) (Integer.parseInt(spinnerAudioFadeOut.getText()) * ((float) 1000 / FFPROBE.currentFPS));
+			long audioStart = (long) FFPROBE.dureeTotale - audioOutValue;
+			
+			if (caseInAndOut.isSelected())
+			{
+				long totalIn = (long) (Integer.parseInt(VideoPlayer.caseInH.getText()) * 3600000 + Integer.parseInt(VideoPlayer.caseInM.getText()) * 60000 + Integer.parseInt(VideoPlayer.caseInS.getText()) * 1000 + Integer.parseInt(VideoPlayer.caseInF.getText()) * (1000 / FFPROBE.currentFPS));
+				long totalOut = (long) (Integer.parseInt(VideoPlayer.caseOutH.getText()) * 3600000 + Integer.parseInt(VideoPlayer.caseOutM.getText()) * 60000 + Integer.parseInt(VideoPlayer.caseOutS.getText()) * 1000 + Integer.parseInt(VideoPlayer.caseOutF.getText()) * (1000 / FFPROBE.currentFPS));
+				 
+				if (VideoPlayer.comboMode.getSelectedItem().toString().contentEquals(Shutter.language.getProperty("cutUpper")))
+				{
+					if (totalIn >= 10000)
+						audioStart = 10000 + (totalOut - totalIn) - audioOutValue;
+					else
+						audioStart = (long) (Integer.parseInt(VideoPlayer.caseOutH.getText()) * 3600000 + Integer.parseInt(VideoPlayer.caseOutM.getText()) * 60000 + Integer.parseInt(VideoPlayer.caseOutS.getText()) * 1000 + Integer.parseInt(VideoPlayer.caseOutF.getText()) * (1000 / FFPROBE.currentFPS)) - audioOutValue;
+				}
+				else //Remove mode
+					audioStart = FFPROBE.dureeTotale - (totalOut - totalIn) - audioOutValue;
+			}
+			
+			audioFilter += ",afade=out:st=" + audioStart + "ms:d=" + audioOutValue + "ms";
+		}
+		
+    	//Audio Speed
 		if (caseConvertAudioFramerate.isSelected())     
         {
         	float AudioFPSIn = Float.parseFloat((comboAudioIn.getSelectedItem().toString()).replace(",", "."));
         	float AudioFPSOut = Float.parseFloat((comboAudioOut.getSelectedItem().toString()).replace(",", "."));
         	float value = (float) (AudioFPSOut / AudioFPSIn);
-        	audioSpeed = ",atempo=" + value;	
+        	audioFilter += ",atempo=" + value;	
         }
 		
-		if (FFPROBE.channels == 1 && lblSplit.getText().equals("Mono"))
+		if (FFPROBE.channels == 1 && lblSplit.getText().equals(language.getProperty("mono")))
 		{			
 			
 			for (int i = 1 ; i < 3; i ++)
@@ -301,7 +400,7 @@ public class FLAC extends Shutter {
 						yesno = " -n ";	
 				}
 				
-				String cmd = " -filter_complex " + '"' + "[a:0]pan=1c|c0=c" + (i - 1) + audioSpeed + "[a" + (i - 1) + "]" + '"' + " -map " + '"'+ "[a" + (i - 1) + "]" + '"' + " -acodec flac -compression_level " + comboFilter.getSelectedItem().toString() + " -vn" + yesno;
+				String cmd = " -filter_complex " + '"' + "[a:0]pan=1c|c0=c" + (i - 1) + audioFilter + "[a" + (i - 1) + "]" + '"' + " -map " + '"'+ "[a" + (i - 1) + "]" + '"' + " -acodec flac -compression_level " + comboFilter.getSelectedItem().toString() + " -vn" + yesno;
 				FFMPEG.run(FFMPEG.inPoint + " -i " + '"' + file.toString() + '"' + FFMPEG.postInPoint + FFMPEG.outPoint + cmd + '"'  + fileOut + '"');	
 				
 				do
@@ -320,7 +419,7 @@ public class FLAC extends Shutter {
 		{
 			JOptionPane.showMessageDialog(Shutter.frame, Shutter.language.getProperty("theFile") + " " + fichier + " " + Shutter.language.getProperty("isAlreadyStereo"), Shutter.language.getProperty("cantSplitAudio"), JOptionPane.ERROR_MESSAGE);
 		}
-		else if (FFPROBE.channels > 1 && lblSplit.getText().equals("Mono"))
+		else if (FFPROBE.channels > 1 && lblSplit.getText().equals(language.getProperty("mono")))
 		{
 			for (int i = 1 ; i < FFPROBE.channels + 1; i ++)
 			{
@@ -334,10 +433,10 @@ public class FLAC extends Shutter {
 						yesno = " -n ";	
 				}
 				
-				if (caseConvertAudioFramerate.isSelected())     
-					audioSpeed = audioSpeed.replace(",", " -filter_complex ");
+				if (audioFilter != "")     
+					audioFilter = audioFilter.replaceFirst(",", " -filter_complex ");
 				
-				String cmd = audioSpeed + " -map a:" + (i - 1) + " -acodec flac -compression_level " + comboFilter.getSelectedItem().toString() + " -vn" + yesno;
+				String cmd = audioFilter + " -map a:" + (i - 1) + " -acodec flac -compression_level " + comboFilter.getSelectedItem().toString() + " -vn" + yesno;
 				FFMPEG.run(FFMPEG.inPoint + " -i " + '"' + file.toString() + '"' + FFMPEG.postInPoint + FFMPEG.outPoint + cmd + '"'  + fileOut + '"');	
 				
 				do
@@ -369,7 +468,7 @@ public class FLAC extends Shutter {
 						yesno = " -n ";	
 				}
 				
-				String cmd = " -filter_complex " + '"' + "[0:a:" + (i - 1) + "][0:a:" + i + "]amerge=inputs=2" + audioSpeed + "[a]" + '"' + " -map " + '"' + "[a]" + '"' + " -acodec flac -compression_level " + comboFilter.getSelectedItem().toString() + " -vn" + yesno;
+				String cmd = " -filter_complex " + '"' + "[0:a:" + (i - 1) + "][0:a:" + i + "]amerge=inputs=2" + audioFilter + "[a]" + '"' + " -map " + '"' + "[a]" + '"' + " -acodec flac -compression_level " + comboFilter.getSelectedItem().toString() + " -vn" + yesno;
 				FFMPEG.run(FFMPEG.inPoint + " -i " + '"' + file.toString() + '"' + FFMPEG.postInPoint + FFMPEG.outPoint + cmd + '"'  + fileOut + '"');	
 				
 				do
