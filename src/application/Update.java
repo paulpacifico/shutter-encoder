8df436095fd565ab934e454c243761b3c2879514
Pacifico Paul <46673657+paulpacifico@users.noreply.github.com>
2021-06-01 09:10:55 +0200

    Add files via upload

--- src/application/Update.java
+++ src/application/Update.java
@@ -273,21 +273,26 @@ public class Update {
 			    	else if (System.getProperty("os.name").contains("Mac")) //MAC
 	            	{
 	            		try {
-	            			String arch = "amd64";
+	            			String arch = "x86_64";
 	            			
 	            			//Checking java amd or arm version
 	            			try {
-	            				Process proc = Runtime.getRuntime().exec("file JRE/Contents/Home/bin/java");	           				
+	            				String PathToJAVA = Shutter.class.getProtectionDomain().getCodeSource().getLocation().getPath();
+	            				PathToJAVA = PathToJAVA.substring(0,PathToJAVA.length()-1);
+	            				PathToJAVA = PathToJAVA.substring(0,(int) (PathToJAVA.lastIndexOf("/"))).replace("%20", " ")  + "/JRE/Contents/Home/bin/java";
+	            				
+	            				ProcessBuilder processJAVA = new ProcessBuilder("file", PathToJAVA);	            				
+	            				Process proc = processJAVA.start();	            		         				
 
 	            		        BufferedReader reader =  new BufferedReader(new InputStreamReader(proc.getInputStream()));	           		       
-
-	            		        String s[] = reader.readLine().split(" ");	           		       
-
+	            		        
+	            		        String s[] = reader.readLine().split(" ");     		       
+	            		        
 	            		        arch = s[s.length - 1];           		       
 
-	            			} catch (Exception e) {}	           			
+	            			} catch (Exception e) {}     
 
-	            			if (arch.equals("amd64")) //AMD64
+	            			if (arch.equals("x86_64")) //AMD64
 	            			{	            			
 			            		if (file.attr("href").contains("Shutter Encoder (MAC Version"))
 			            		{
